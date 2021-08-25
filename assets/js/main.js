735f15df56335ae65ff986f0baa09344349e46e1
Isaac Fierro <isaaczid@hotmail.com>
2021-08-25 09:27:24 -0700

    Selector de boton cambiado

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,5 @@ import menuHamburguesa from "./modulos/menu-hamburguesa.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
-    menuHamburguesa(".boton", ".panel");
+    menuHamburguesa(".hamburger", ".panel");
 });
