61ee8f8701cbef584122fe9a9d1c6e40a69d6e13
Isaac Fierro <isaaczid@hotmail.com>
2021-08-24 21:39:42 -0700

    Eventos del teclado terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 import menuHamburguesa from "./modulos/menu-hamburguesa.js";
 import relojAlarma from "./modulos/reloj-alarma.js";
+import { moverPelota, atajosTeclado } from "./modulos/eventos-teclado.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
     menuHamburguesa(".boton", ".panel");
     relojAlarma(".reloj__display", ".reloj__boton");
+    moverPelota(".campo", ".pelota");
+    atajosTeclado();
 });
---
+++ assets/js/modulos/eventos-teclado.js
@@ -0,0 +1,51 @@
+const d = document;
+let posX = 0,
+    posY = 0;
+
+
+export function moverPelota(selecCampo, selecPelota){
+    d.addEventListener("keydown", e => {
+        const $campo = d.querySelector(selecCampo),
+            $pelota = d.querySelector(selecPelota),
+            posicionCampo = $campo.getBoundingClientRect(),
+            posicionPelota = $pelota.getBoundingClientRect();
+
+        switch(e.key){
+            case "ArrowUp": 
+                if(posicionPelota.top > posicionCampo.top){
+                    console.log("Flecha arriba");
+                    posY--;
+                    e.preventDefault();
+                }
+                break;
+            case "ArrowDown": 
+                if(posicionPelota.bottom < posicionCampo.bottom){
+                    console.log("Flecha abajo");
+                    posY++;
+                    e.preventDefault();
+                }
+                break;
+            case "ArrowLeft":
+                if(posicionPelota.left > posicionCampo.left){
+                    posX--;
+                }
+                break;
+            case "ArrowRight":
+                if(posicionPelota.right < posicionCampo.right){
+                    posX++;
+                }
+                break; 
+        }
+
+        $pelota.style.setProperty("transform", `translate(${posX * 10}px, ${posY * 10}px)`);
+    });
+}
+
+export function atajosTeclado(){
+    d.addEventListener("keypress", e => {
+        console.log(e);
+        if(e.shiftKey && e.key === "C"){
+            alert("Hola como estas usuarios :P");
+        }
+    });
+}
